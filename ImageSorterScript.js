712620f14824a97f9396126df010898d48440767
Hexy <62488213+hexyon@users.noreply.github.com>
2024-01-07 20:28:26 +0300

    Add files via upload

---
+++ ImageSorterScript.js
@@ -0,0 +1,37 @@
+let smallImages = [];
+let largeImages = [];
+
+function resizePhotos(size) {
+    let uploadButton = document.getElementById(size === 'small' ? 'uploadButton1' : 'uploadButton2');
+    let files = uploadButton.files;
+
+    for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+        let img = new Image();
+
+        img.onload = function() {
+            if ((size === 'small' && this.width < 1000 && this.height < 1000) ||
+                (size === 'large' && this.width >= 1000 && this.height >= 1000)) {
+                let images = size === 'small' ? smallImages : largeImages;
+                images.push(URL.createObjectURL(file));
+                document.getElementById('count' + (size === 'small' ? '1' : '2')).innerText = images.length + ' ' + size + ' images';
+            }
+        };
+
+        img.src = URL.createObjectURL(file);
+    }
+}
+
+function downloadImages(size) {
+    let images = size === 'small' ? smallImages : largeImages;
+
+    for (let i = 0; i < images.length; i++) {
+        let a = document.createElement('a');
+        a.href = images[i];
+        a.download = 'image' + i;
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+}
